8636b5720fc448f93b2ada90f4212147581f962e
André P. Santos <andreztz@gmail.com>
2023-10-28 23:09:12 -0300

    Change colors

--- src/git-status.sh
+++ src/git-status.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 cd $1
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
-STATUS=$(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)
+STATUS=$(git status --porcelain 2>/dev/null | egrep "^(M| M)" | wc -l)
 if test "$BRANCH" != ""; then
   if test "$STATUS" = "0"; then
-    echo "#[fg=green,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=green,bold]   $BRANCH  "
+    echo "#[fg=#00FF00,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=#00FF00,bold]   $BRANCH  "
   else
-    echo "#[fg=red,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=red,bold]   $BRANCH  "
+    echo "#[fg=#FF00FF,bg=#24283B,nobold,noitalics,nounderscore] #[fg=black,bg=#FF00FF,bold]   $BRANCH  "
   fi
 fi
